51e28099b03b5fe3f5683719f89bfdd5883bca0c
soumik0x0 <iamrealsoumik@gmail.com>
2026-01-29 17:01:29 +0530

    29 Jan 2026

---
+++ Strings/compareTo.java
@@ -0,0 +1,9 @@
+public class compareTo {
+  public static void main(String[] args) {
+    String str1 = "Telusko";
+    String str2 = "Telusko";
+    String str3 = "Alien";
+
+    System.out.println(str1.compareTo(str3));
+  }
+}
